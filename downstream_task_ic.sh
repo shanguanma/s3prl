cae3fe80ca476da39a65de03eecd0217f2f98dcb
shanguanma <maduo25@163.com>
2023-04-20 16:11:25 +0800

    add install shell script and downstream task full shell script

---
+++ downstream_task_ic.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+stage=-1
+stop_stage=1000
+. utils/parse_options.sh
+. path_for_fairseq_speechtext.sh ## full install fairseq and s3prl project
+
+if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ];then
+    pretrain_model=exp/pretrain/pretrain_on_hubert_4gpu_8update_960h_mfcc_250k_update/checkpoint_289_400000.pt
+    exp_dir=exp/downstream_task/downstream_task_ic
+    python   s3prl/s3prl/upstream/hubert/convert.py\
+               --fairseq_ckpt   $pretrain_model\
+               --output_dir $exp_dir
+                
+fi
+
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ];then
+   exp_dir=exp/downstream_task/downstream_task_ic
+   pretrain_model=$exp_dir/checkpoint_289_400000.pt
+   num_gpus=4
+   downstream_task_config=s3prl/s3prl/downstream/fluent_commands/config.yaml
+   CUDA_VISIBLE_DEVICES=0,1,2,3 python -m torch.distributed.launch --use_env --nproc_per_node $num_gpus s3prl/s3prl/run_downstream.py\
+           -n $exp_dir\
+           -m train \
+           -u hubert_local\
+           -k $pretrain_model\
+           -d fluent_commands\
+           --config $downstream_task_config\
+           -o config.runner.gradient_accumulate_steps=1\
+           --auto_resume 
+fi
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+pip install -e ".[all]" -i https://pypi.tuna.tsinghua.edu.cn/simple
+#pip install -e . -i https://pypi.tuna.tsinghua.edu.cn/simple
